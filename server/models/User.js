731eb5c52101a20f7dc1b359d32207c2df78877c
Marcelo-Diament <marcelodiament@gmail.com>
2021-10-27 20:50:14 -0300

    feature: User model finalizado

--- server/models/User.js
+++ server/models/User.js
@@ -10,27 +10,64 @@ module.exports = (sequelize, DataType) => {
       type: DataType.STRING(50),
       allowNull: false
     },
-    sobrenome,
-    apelido,
+    sobrenome: {
+      type: DataType.STRING(50),
+      allowNull: false
+    },
+    apelido: {
+      type: DataType.STRING(50),
+      allowNull: true
+    },
     nascimento: {
       type: DataType.DATEONLY,
       allowNull: true
     },
-    senha,
-    corPreferida,
-    avatar,
-    email,
-    telefone,
+    senha: {
+      type: DataType.STRING(512),
+      allowNull: false
+    },
+    corPreferida: {
+      type: DataType.STRING(7),
+      allowNull: false
+    },
+    avatar: {
+      type: DataType.STRING(150),
+      allowNull: true,
+      defaultValue: '/images/user-placeholder.png'
+    },
+    email: {
+      type: DataType.STRING(150),
+      allowNull: false
+    },
+    telefone: {
+      type: DataType.STRING(13),
+      allowNull: true
+    },
     bio: {
       type: DataType.TEXT,
       allowNull: true
     },
-    plano_id,
-    papel_id,
+    plano_id: {
+      type: DataType.INTEGER.UNSIGNED,
+      allowNull: false,
+      defaultValue: 1
+    },
+    papel_id: {
+      type: DataType.INTEGER.UNSIGNED,
+      allowNull: false,
+      defaultValue: 2
+    },
     criadoEm: {
       type: DataType.DATE,
       allowNull: false
     },
-    modificadoEm
+    modificadoEm: {
+      type: DataType.DATE,
+      allowNull: false
+    }
+  }, {
+    tableName: 'users',
+    timestamps: false
   })
+  return User
 }
